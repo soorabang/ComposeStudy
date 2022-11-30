36f5bd2170d947b762d903697f93a45ec5f32c20
방수라 <soora.bang@42dot.ai>
2022-11-30 20:39:43 +0900

    list scroll status

--- app/src/main/java/soora/example/composelisttest/ListComponent.kt
+++ app/src/main/java/soora/example/composelisttest/ListComponent.kt
@@ -2,11 +2,15 @@ package soora.example.composelisttest
 
 import android.annotation.SuppressLint
 import android.util.Log
+import android.view.MotionEvent
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,13 +21,21 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
@@ -40,6 +52,39 @@ fun TestListView(
             listState.animateScrollToItem(viewModel.uiState.scrollToPosition)
         }
     }
+
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                Log.e("soora", "onPreScroll ${listState.isScrollInProgress}")
+                //listState.isScrollInProgress- true 계속 호출됨.  처음 한번만 처리하도록 해야함.
+                return super.onPreScroll(available, source)
+            }
+
+            override fun onPostScroll(
+                consumed: Offset,
+                available: Offset,
+                source: NestedScrollSource
+            ): Offset {
+                Log.e("soora", "onPostScroll")
+                return super.onPostScroll(consumed, available, source)
+            }
+
+            override suspend fun onPreFling(available: Velocity): Velocity {
+                Log.e("soora", "onPreFling")
+                return super.onPreFling(available)
+            }
+
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                Log.e("soora", "onPostFling  ${listState.isScrollInProgress}")
+                // listState.isScrollInProgress - false
+                // SCROLL_STATE_IDLE
+
+                return super.onPostFling(consumed, available)
+            }
+        }
+    }
+
     Column() {
         Row(modifier = Modifier.fillMaxWidth()) {
             Button(
@@ -72,16 +117,29 @@ fun TestListView(
                 Text("Refresh List")
             }
         }
-        LazyColumn(state = listState) {
-            items(count) {
-                TestListItem(
-                    order = it,
-                    focused = viewModel.uiState.selectedPosition == -1 || viewModel.uiState.selectedPosition == it,
-                    1f,
-                    clickListener,
-                )
+        //https://blog.onebone.me/post/jetpack-compose-nested-scroll/
+        // pointerInteropFilter 는 Box에 넣든, LazyColumn에 넣든 중간에 DOWN 만 계속 찍히면서 MOVE가 먹는 현상이 있음.
+        Box(Modifier.nestedScroll(nestedScrollConnection)/*.pointerInteropFilter {
+            when(it.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d("soora", "ACTION_DOWN")
+                }
+                else -> {}
+            }
+            true
+        }*/) {
+            LazyColumn(state = listState) {
+                items(count) {
+                    TestListItem(
+                        order = it,
+                        focused = viewModel.uiState.selectedPosition == -1 || viewModel.uiState.selectedPosition == it,
+                        1f,
+                        clickListener,
+                    )
+                }
             }
         }
+
     }
 }
 
@@ -136,7 +194,6 @@ fun TestListItem(
             Text(text = listtext, textAlign = TextAlign.Center)
         }
 
-        Log.d("TEST", "order:$order, selected:$selected, progress:$progress, progressTargetValue:$progressTargetValue")
         if (selected) {
             LinearProgressIndicator(
                 modifier = Modifier
@@ -144,7 +201,7 @@ fun TestListItem(
                     .height(100.dp)
                     .alpha(if (selected) 0.5f else 0f),
                 trackColor = Color.Transparent,
-                color = if (selected) Color.LightGray else Color.Transparent , //progress color,
+                color = if (selected) Color.LightGray else Color.Transparent, //progress color,
                 progress = progressAnimation
             )
         }
